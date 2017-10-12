c46a79d64788ecc46f435ca35000768ad9342a6f
Ingryd Moura <ingrydmoura27@hotmail.com>
2017-10-12 13:29:33 -0300

    Tarefa-04 TERMINADA

--- tarefas/tarefa-04/conf.lua
+++ tarefas/tarefa-04/conf.lua
@@ -1,4 +1,5 @@
 function love.conf(t)
-    t.title = "The Loving cat!" 
-    
+	t.title = "Jumping cat!" 	
+	t.window.width = 800        
+	t.window.height = 700
 end
--- tarefas/tarefa-04/main.lua
+++ tarefas/tarefa-04/main.lua
@@ -1,126 +1,130 @@
 
-player = {}
-cat2={}
-
-tile = {}
-for i=1,29 do 
-    tile[i] = love.graphics.newImage( "plat"..i..".png")   
-end
- 
-  
-
-
- 
-function love.load()
-    
-
+obstaculos = {}
+player = { 
+	x = 100, 
+	y = 640,
+	jump = false, 
+	accel = aceleracao, 
 	
-  cat2.img = love.graphics.newImage('catGirl.png')
-  cat2.x = 560
-  cat2.y = 560
-    
-  player.img = love.graphics.newImage('cat1.png')
-	player.x = 25
-	player.y = love.graphics.getHeight() - 20
- 
-	player.speed = 500
- 
-    
+}
 
-    
+
+
+obstTemp = 10 
+gravity = 100
+aceleracao = 16
+gravid = 200 
+cont = 0
+gameover = false
+
+
+function love.load()
+   fundo = love.graphics.newImage("fundo.jpg")
+   player.img = love.graphics.newImage("cat1.png")  
+   font = love.graphics.newFont(40)
+
 end
  
 function love.update(dt)
-
-
-	if love.keyboard.isDown('d') then
-		  player.img = love.graphics.newImage('cat1.png')
-			player.x = player.x + (player.speed * dt)
-      
-      if player.x > love.graphics.getWidth()- 40 then
-          player.x = love.graphics.getWidth() - 40
-      end
-      
-      
-  end
-  if love.keyboard.isDown('s') then   
-      player.img = love.graphics.newImage('cat4.png')
-      player.y = player.y + (player.speed * dt)
-     
-      
-      if player.y > love.graphics.getHeight() then
-          player.y = love.graphics.getHeight() - 10
-      end 
-  end  
-	if love.keyboard.isDown('a') then
-		  player.img = love.graphics.newImage('cat3.png')
-			player.x = player.x - (player.speed * dt)
-      
-      if player.x < 60 then
-          player.x = 60
-      end
-      
-      
-  end
-  if love.keyboard.isDown('w') then
-    player.img = love.graphics.newImage('cat2.png')
-    player.y = player.y -(player.speed * dt)
-    
-      if player.y < love.graphics.getHeight() - 500   then
-          player.y = love.graphics.getHeight() - 500
-      end 
-  end
-
-
-  if love.keyboard.isDown('escape') then
-    love.event.push('quit')
-  end
-    
-    
-for i=1,29 do     
-if player.x + player.img:getWidth() > tile[i].x and player.y < player.img:getHeight() + tile[i].y then -- Prevents teleporting to the top of the platform
-  
-          player.x = tile[i].x - player.img:getWidth() - 1 
-
-          player.x = tile[i].x + tile[i].y + 1 
-end
-end
-    
- -- CheckCollision(player.x,player.y,player.img:getWidth(),player.img:getHeight(),tile.x,tile.y,tile.width,tile.height)
-   
+   -- Sair do jogo --
+   if love.keyboard.isDown('escape') then
+	love.event.push('quit')
+   end   
  
-	 	
+	
+	if gameover == false then
+		--Movimentação --
+		if love.keyboard.isDown('space') and player.jump == false then
+			player.jump = true
+		end
+ 
+		
+		if player.jump and player.accel > 0 then
+			player.y = player.y - player.accel
+			player.accel = player.accel - 1
+		end
+ 
+		
+		if player.y < 640 then
+			player.y = player.y + gravity*dt;
+			gravity = gravity + 10;
+		end
+ 
+		if player.y > 640 then 
+			player.y = 640
+		end
+ 
+		if player.y == 640 then
+			player.jump = false
+			player.accel = aceleracao
+			gravity = gravid
+		end
+ 
+		
+		obstTemp = obstTemp - 10
+		if obstTemp <= 0 then
+			obstTemp = love.math.random(200,700)
+ 
+			newObstacle= { x = 650, y = 650, width = 25, height = 50, counted = false}
+			table.insert(obstaculos, newObstacle)
+		end
+		
+		
+		for i, obstacle in ipairs(obstaculos) do
+			obstacle.x = obstacle.x - 10
+			
+			if obstacle.x < 0 then
+				table.remove(obstacle, i)
+			end
+ 
+			
+			if obstacle.counted == false and obstacle.x < player.x then
+				obstacle.counted = true
+				cont = cont + 1
+				
+			end
+		end
+ 
+		-- Colisões --
+		for i, obstacle in ipairs(obstaculos) do
+			if CheckCollision(player.x,player.y,player.img:getWidth(),player.img:getHeight(),obstacle.x,obstacle.y,obstacle.width,obstacle.height) then
+				gameover = true
+				
+			end
+		end
+	end
 end
 
 
---[[
 function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-   for i, tile in ipairs(tile) do
-      if x1==x2 and y1==y2 then
-        x1 = x2 - w2
-        y1 = y2 - h2
-      end
-
-   end 
-         
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
 end
-
-
-]]
  
-function love.draw()  	
+function love.draw()
+	
+	--fundo--
+	love.graphics.draw(fundo,0,0)
 
-    fundo = love.graphics.newImage("fundo.jpg")  
-    love.graphics.draw(fundo,0,0) 
+	--player
+    love.graphics.draw(player.img, player.x, player.y,0,1.8,1.8,0)
  
-    --love.graphics.draw(platform.img, platform.x, platform.y, 800, 31)
-    for i=1,29 do 
-       love.graphics.draw(tile[i], 0, 0)
-    end
     
-
-    love.graphics.draw(cat2.img, cat2.x, cat2.y, 0, 1.5, 1.5, -25, 250)
-    love.graphics.draw(player.img, player.x, player.y, 0, 1.5, 1.5, 15, 50)
-    
+    --Fonte do Contador --
+    love.graphics.setFont(font)
+   	love.graphics.print(cont, 400, 300)	
+ 
+ 	-- Texto Game Over --
+   	if (gameover) then
+   		love.graphics.setColor(94,117,113)
+   		love.graphics.print("GAME OVER", 300, 100)	
+   	end
+ 
+    -- Obstaculos ---
+	for i, obstacle in ipairs(obstaculos) do	  
+	  love.graphics.rectangle('fill', obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+	end
 
 end
